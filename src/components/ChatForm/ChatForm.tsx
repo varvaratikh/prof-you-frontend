d5622851fdb033c4e79eb8dd7b3ed35052f07674
varvaratikh <tikhonovavarvara181203@gmail.com>
2025-01-27 13:15:35 +0300

    started laying out the chat page

---
+++ src/components/ChatForm/ChatForm.tsx
@@ -0,0 +1,8 @@
+import './chatForm.scss';
+export const ChatForm = () => {
+    return(
+        <>
+            <div className="shape"></div>
+        </>
+    )
+}
--- src/pages/Home.tsx
+++
@@ -1,5 +0,0 @@
-const Home: React.FC = () => {
-    return <h1>Home Page</h1>;
-};
-
-export default Home;
