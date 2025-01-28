f3cc293bcd07baf7f06f09bb26784ff8be8296a3
varvaratikh <tikhonovavarvara181203@gmail.com>
2025-01-28 12:16:19 +0300

    started chat's page

--- src/components/ChatForm/ChatForm.tsx
+++ src/components/ChatForm/ChatForm.tsx
@@ -2,7 +2,10 @@ import './chatForm.scss';
 export const ChatForm = () => {
     return(
         <>
-            <div className="shape"></div>
+            <div className="shape">
+                <p className="test_text">Пройдите небольшой тест, пока ИИ анализирует ваше фото!</p>
+                <p className="testSm_text">*Данные прохождения теста не будут сохранены или использованы в каких-либо целях. Все данные конфиденциальны и автоматически удаляются после генерации ответа.</p>
+            </div>
         </>
     )
 }
