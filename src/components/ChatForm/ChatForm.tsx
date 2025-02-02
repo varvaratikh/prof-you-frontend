1ed3d9ccab9cdba0e0969964788f1626d86920f6
varvaratikh <tikhonovavarvara181203@gmail.com>
2025-02-02 14:20:04 +0300

    correct position of the bottom of the chat form

--- src/components/ChatForm/ChatForm.tsx
+++ src/components/ChatForm/ChatForm.tsx
@@ -47,8 +47,10 @@ export const ChatForm = () => {
                     <div className="progress-bar">10%</div>
                 </div>
             </div>
-            <p className="testSm_text">*Данные прохождения теста не будут сохранены или использованы в каких-либо целях. Все данные конфиденциальны и автоматически удаляются после генерации ответа.</p>
-            <button className="ready-btn">ГОТОВО!</button>
+            <div className="test-footer">
+                <p className="testSm_text">*Данные прохождения теста не будут сохранены или использованы в каких-либо целях. Все данные конфиденциальны и автоматически удаляются после генерации ответа.</p>
+                <button className="ready-btn">ГОТОВО!</button>
+            </div>
         </div>
     );
 };
